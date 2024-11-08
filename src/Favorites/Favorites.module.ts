74eba3f996df6beb62613b6a5d8e98bcd1ef2511
Kavin Dail <kavindail17@gmail.com>
2024-11-07 19:58:44 -0800

    Fixed bug where auth module not imported

--- src/Favorites/Favorites.module.ts
+++ src/Favorites/Favorites.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Favorite } from './Favorites.entity';
 import { FavoritesController } from './Favorites.controller';
 import { FavoriteService } from './Favorites.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Favorite])],
+  imports: [TypeOrmModule.forFeature([Favorite]), AuthModule],
   controllers: [FavoritesController],
   providers: [FavoriteService],
   exports: [FavoriteService],
